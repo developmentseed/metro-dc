06f064c19d3dde94d36c23d071763beeab9ed47d
Caroline Portugal <caroline@developmentseed.org>
2015-05-04 10:17:57 -0400

    recovering history from bad merge

--- js/app.js
+++ js/app.js
@@ -3,9 +3,23 @@ $(document).ready(function() {
 // run the init function every 'x' seconds
 setInterval(function(){ 
   init();
-}, 3000);
+}, 5000);
 
+// loop over all the transfer divs and call the draw function for each one
+$('.transfer').each(function(index, value) {
+ var station = $(this).attr('data-official-title'); 
+ var transferElement = $(this);
+ drawTransfers(station, transferElement);
+});
 
+// redraw transfer divs on window resize
+$(window).resize(function() {
+ $('.transfer').each(function(index, value) {
+ var station = $(this).attr('data-official-title'); 
+ var transferElement = $(this);
+ drawTransfers(station, transferElement);
+ });
+});
 
 function init() {
 
@@ -28,20 +42,26 @@ $.ajax({
           
           // group data by line
           var trains = _.groupBy(data.Trains, 'Line');
-          var stations = '';
+          var stations = [];
           var boarding = [];
+          var counter = 0;
+          
           
           // for each line, group data by station
           $.each(trains, function(index, value) {
             //stations = _.groupBy(value, 'LocationName');
             var times = _.groupBy(value, 'Min');
             boarding.push(times);
-            
+            var names = _.groupBy(value, 'LocationName');
+            stations.push(names);
           });
           
           
-            
+          
+          //console.log(stations); 
   			
+  			
+  		
           
           // For each line go through and calculate number of trains
  		  // Attach train to their lines and direction
@@ -52,8 +72,11 @@ $.ajax({
             
              $.each(value.BRD, function(index, train) {
                $('.train-line ul li').each(function(index, station) {
-                  var stationName = $(this).text();
-                  if (stationName === train.LocationName) {
+                  var lineColor = $(this).parent('ul').attr('data-line');
+                  var stationName = $(this).attr('data-official-title');
+                  if (stationName === train.LocationName && lineColor === train.Line) {
+                      counter++;
+                       $('.counter').text(counter);
                     if (train.Group === '1') {
                       $(this).append('<div class="train outbound-train">train</div>');
                     } else {
@@ -66,6 +89,8 @@ $.ajax({
           
           
           
+    
+  			
           
          
       }
@@ -73,12 +98,28 @@ $.ajax({
  
 
 }
- 
-   //For each train, look at wait-time value and Location Name
-  // Update train positon to location name, offset position based on wait-time/distance from stations (75px) and direction
-  
-   
- // If wait-time=boarding, make train blink       
 
-
+		function drawTransfers(stationTitle, transferElement) {
+    
+    	// for each station where the official name = X draw an absolute container element
+        var transfer = $('li[data-official-title="' + stationTitle + '"]');
+        console.log(transfer);
+        var transferLength = transfer.length -1;
+        //console.log(transferLength);
+        var colWidth = $('.col-md-2').width();
+        var topLeft = $(transfer[0]).offset(); 
+        var topRight = $(transfer[transferLength]).offset();
+        var width = topRight.left + colWidth - topLeft.left;
+       
+        
+        // update positions
+        $(transferElement).css('top', topLeft.top + 'px');
+        $(transferElement).css('left', topLeft.left + 'px');
+        $(transferElement).css('width', width + 'px');
+         //console.log(topRight);  
+          //console.log(topLeft); 
+          
+        }
+          
+          
 });
